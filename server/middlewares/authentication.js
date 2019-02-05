24ad69079c882789d092311712024e03e756e898
Miguel Benjamin Atencio Vargas <atencio947@gmail.com>
2019-02-05 19:00:47 -0400

    Login usuario final seccion 10

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,41 @@
+//=====
+// Verificar token
+//====
+const jwt = require('jsonwebtoken');
+
+let verificarToken = (req, res, next) => {
+    let token = req.get('Authorization');
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err
+            })
+        }
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
+/*===========
+ Verifica AdminRole
+==========*/
+let verificaAdmin_Role = (req, res, next) => {
+
+    let usuario = req.usuario;
+    console.log(usuario);
+    if (!(usuario.role === 'ADMIN_ROLE')) {
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'Su usuario no es el de un administrador'
+            }
+        });
+    }
+    next();
+}
+
+module.exports = {
+    verificarToken,
+    verificaAdmin_Role
+}
